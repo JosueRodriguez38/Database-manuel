570d3e0618f26f0da682512a6077daf45da3d2d3
iandres1 <Andrwe09>
2020-03-15 14:16:28 -0400

    no se si se use

---
+++ handler/transaction.py
@@ -0,0 +1,12 @@
+from flask import jsonify
+from dao.transaction import TransactionDAO
+
+class TransactionHandler:
+    def build_transaction_dict(self, row):
+        result = {}
+        result['tid'] = row[0]
+        resilt['cid'] = row[1]
+        result['cost'] = row[2]
+        result['paymentMethod'] = row[3]
+        return result
+
