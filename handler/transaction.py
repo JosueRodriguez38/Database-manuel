6417a35ab54359c0c84a3e97dd08997d6ea51719
EmmanuelAlonso <emmanuel.alonso@upr.edu>
2020-03-15 17:13:24 -0400

    update

--- handler/transaction.py
+++ handler/transaction.py
@@ -5,7 +5,7 @@ class TransactionHandler:
     def build_transaction_dict(self, row):
         result = {}
         result['tid'] = row[0]
-        resilt['cid'] = row[1]
+        result['cid'] = row[1]
         result['cost'] = row[2]
         result['paymentMethod'] = row[3]
         return result
