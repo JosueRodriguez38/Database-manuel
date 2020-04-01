8fe3cbbdb5aedeefff12ec8da6132dc0cc9042b4
EmmanuelAlonso <emmanuel.alonso@upr.edu>
2020-03-31 21:00:29 -0400

    All comments added in rest of DAO classes

--- dao/resource.py
+++ dao/resource.py
@@ -2,6 +2,7 @@ from config.dbconfig import pg_config
 from config.tuple_config import resource
 import psycopg2
 
+# The purpose of the resource DAO is to extract the information of a resource that has been requested
 
 class ResourcesDAO:
     def __init__(self):
@@ -11,6 +12,7 @@ class ResourcesDAO:
                                                                            pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
 
+    # returns all the resources in the Resources table
     def getAllResources(self):
         result = []
         r = []
@@ -19,12 +21,14 @@ class ResourcesDAO:
         result.append(r)
         return result
 
+    # Resources have an id, this functions finds the resource with the id input, if it exists
     def getResourceById(self, pid):
         if pid == resource[0]:
             return resource
         else:
             return
 
+    # uses the input resource id to find the suppliers that supplies a specific resource
     def getSuppliersByResourcesId(self, pid):
         cursor = self.conn.cursor()
         query = "select sid, sname, scity, sphone from supplies natural inner join supplier natural inner join supplies where pid = %s;"
@@ -34,6 +38,7 @@ class ResourcesDAO:
             result.append(row)
         return result
 
+    # returns the resources with the same name and cost as the input
     def getResourcesByNameAndCost(self, name, cost):
         result = []
 
@@ -42,6 +47,7 @@ class ResourcesDAO:
 
         return result
 
+    # returns the resource with the name that is used in the input
     def getResourcesByName(self,name):
         result = []
 
@@ -50,6 +56,7 @@ class ResourcesDAO:
 
         return result
 
+    # returns the resource with a specified cost
     def getResourcesByCost(self,cost):
         result = []
 
@@ -58,12 +65,14 @@ class ResourcesDAO:
 
         return result
 
+    # inserts a resource with its id, supplier id, name, cost, and reserved amount
     def insert(self,sid, rname, cost, resv_amount):
 
         if sid == resource[1]:
             return resource[0]
         return
 
+    # deletes a resource (specified by id) if it exists
     def delete(self, rid):
 
         if rid==resource[0]:
