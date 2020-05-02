155eed5b3e4866f01c2ef8c772155fc43aeb2808
EmmanuelAlonso <emmanuel.alonso@upr.edu>
2020-05-02 17:19:39 -0400

    Adding Clases to dao file this dao clases need to be implemented acording to their unic uses 1 dao class for each resource table

---
+++ dao/Fuel.py
@@ -0,0 +1,12 @@
+from config.dbconfig import pg_config
+from config.tuple_config import user_cons
+import psycopg2
+
+# The purpose of the consumer DAO is to extract the information regarding a consumer that has been requested
+
+class FuelDAO:
+    def __init__(self):
+        connection_url = "dbname=%s user=%s password=%s host=24.54.205.36" % (pg_config['dbname'],
+                                                                           pg_config['user'],
+                                                                           pg_config['passwd'])
+        self.conn = psycopg2._connect(connection_url)
