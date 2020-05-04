cea654ec0e1e6685e7ddca5ab20d18f420ffd71b
EmmanuelAlonso <emmanuel.alonso@upr.edu>
2020-05-04 05:41:32 -0400

    implementing some query inside water dao

--- dao/Water.py
+++ dao/Water.py
@@ -10,3 +10,78 @@ class WaterDAO:
                                                                            pg_config['user'],
                                                                            pg_config['passwd'])
         self.conn = psycopg2._connect(connection_url)
+
+    def getAllWater(self):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getWaterbyResourceId(self,resourceid):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources where resourceid=%s;"
+        cursor.execute(query,resourceid)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+        return
+
+    def getAllWaterbyType(self,watertypenumber):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources where watertypenumber=%i;"
+        cursor.execute(query,watertypenumber)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getWaterByOunces(self,ounces):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources where onces=%f;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getWaterOrderedByOunceDes(self):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources order by ounces desc;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+
+    def getWaterOrderedByOunceAsc(self):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources order by ounces Asc;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getWaterOrderedByTypeAsc(self):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources order by watertypenumber Asc;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
+
+    def getWaterOrderedByTypeDesc(self):
+        cursor = self.conn.cursor()
+        query = "select ammount,cost,watertypename, ounces from (water natural inner join water_type)natural inner join resources order by watertypenumber desc;"
+        cursor.execute(query)
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
